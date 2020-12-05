d8762df24986bacfa49ea5e66971c2a314d7ca10
Marco Tarantino <taran.marco@gmail.com>
2020-12-05 12:47:43 +0100

    ES1 part2 done

--- src/AOC/ES1.hs
+++ src/AOC/ES1.hs
@@ -21,12 +21,38 @@ parseInput ss = do
         loop [] = pure []
     loop ss
 
+getTriples :: [a] -> [(a, a, a)]
+getTriples [] = []
+getTriples [_] = []
+getTriples [_, _] = []
+getTriples (x : xs) =
+    triples x (getPairs xs)
+        ++ getTriples xs
+  where
+    triples z = map (\(s, t) -> (z, s, t))
+
+filterBySum3 :: (Ord a, Num a) => a -> [(a, a, a)] -> [(a, a, a)]
+filterBySum3 tot = filter (\(x, y, z) -> x + y + z == tot)
+
 main :: FilePath -> IO ()
 main input = do
     inputLines <- lines <$> readFile input
     parsed <- parseInput inputLines
     let results = filterBySum 2020 $ getPairs parsed
+    putStrLn "First Part:"
     case results of
-        [] -> putStrLn "No pair found"
+        [] -> putStrLn "No triple found"
         [(x, y)] -> putStrLn $ "Result: " ++ show (x * y)
         _ -> putStrLn "Multiple matches found"
+
+    let triples = getTriples parsed
+        res2 = filterBySum3 2020 triples
+
+    putStrLn "Second Part:"
+    putStrLn $ "n. of elements: " ++ show (length parsed)
+    putStrLn $ "Found triples: " ++ show (length triples)
+
+    case res2 of
+        [] -> putStrLn "No triple found"
+        [(x, y, z)] -> putStrLn $ "Result: " ++ show (x * y * z)
+        e -> putStrLn $ "Multiple matches found: " ++ show e
