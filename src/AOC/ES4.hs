0f7a58503237c827a629f72fc4ee08637277aa67
Marco Tarantino <taran.marco@gmail.com>
2020-12-05 20:06:46 +0100

    ES2 part2 done

--- src/AOC/ES4.hs
+++ src/AOC/ES4.hs
@@ -1,17 +1,104 @@
 module AOC.ES4 (main) where
 
 import AOC.Utils
+import Text.Read (readMaybe)
+import Text.Regex.TDFA
 
 type Passport = [(String, String)]
 
 psptRequiredKeys :: [String]
 psptRequiredKeys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
+type Match = (String, String, String, [String])
+
+parseRegexReader :: MonadFail m => String -> String -> (String -> m a) -> m a
+parseRegexReader val regex parser = do
+    (_, m, _, _) :: Match <- val =~~ regex
+    if m /= ""
+        then parser m
+        else fail "Cannot parse"
+
+parseRegexString :: String -> String -> Maybe String
+parseRegexString regex val = parseRegexReader val regex $ \x -> pure x
+
+parseRegexInt :: String -> String -> Maybe Int
+parseRegexInt regex val = parseRegexReader val regex readMaybe
+
+mkValidator :: (String -> Maybe a) -> (a -> Bool) -> String -> Bool
+mkValidator parser validator x = maybe False validator (parser x)
+
+mkSimpleValidator :: (String -> Maybe a) -> String -> Bool
+mkSimpleValidator parser = mkValidator parser $ const True
+
+parseFourDigits :: String -> Maybe Int
+parseFourDigits = parseRegexInt "^[0-9][0-9][0-9][0-9]$"
+
+psptValidationExpr :: [(String, String -> Bool)]
+psptValidationExpr =
+    [
+        ( "byr"
+        , let validator x = x >= 1920 && x <= 2002
+              parser = parseFourDigits
+           in mkValidator parser validator
+        )
+    ,
+        ( "iyr"
+        , let validator x = x >= 2010 && x <= 2020
+              parser = parseFourDigits
+           in mkValidator parser validator
+        )
+    ,
+        ( "eyr"
+        , let validator x = x >= 2020 && x <= 2030
+              parser = parseFourDigits
+           in mkValidator parser validator
+        )
+    ,
+        ( "hgt"
+        , let validCm x = x >= 150 && x <= 193
+              validIn x = x >= 59 && x <= 76
+              parser val = do
+                (_, _, _, [h, u]) :: Match <- val =~~ ("^([0-9]+)(cm|in)$" :: String)
+                height :: Int <- readMaybe h
+                case u of
+                    "cm" -> if validCm height then pure height else Nothing
+                    "in" -> if validIn height then pure height else Nothing
+                    _ -> Nothing
+           in mkSimpleValidator parser
+        )
+    ,
+        ( "hcl"
+        , let parser = parseRegexString "^#[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]$"
+           in mkSimpleValidator parser
+        )
+    ,
+        ( "ecl"
+        , let parser = parseRegexString "^(amb|blu|brn|gry|grn|hzl|oth)$"
+           in mkSimpleValidator parser
+        )
+    ,
+        ( "pid"
+        , let parser = parseRegexInt "^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$"
+           in mkSimpleValidator parser
+        )
+    ]
+
 isValid1 :: Passport -> Bool
 isValid1 pspt =
     let keys = map fst pspt
         values = map snd pspt
      in all (`elem` keys) psptRequiredKeys && notElem "" values
+
+validateKey :: (String, String -> Bool) -> Passport -> Bool
+validateKey (key, validator) pspt =
+    let keyarr = filter ((== key) . fst) pspt
+        value = snd $ head keyarr
+     in validator value
+
+isValid2 :: Passport -> Bool
+isValid2 pspt =
+    let keys = map fst pspt
+     in all (`elem` keys) psptRequiredKeys && all (`validateKey` pspt) psptValidationExpr
 
 parsePassport :: [String] -> IO Passport
 parsePassport [] = pure []
@@ -33,3 +120,5 @@ main fp = do
     passports <- parseList parsePassport splitData
     let validPassports = filter isValid1 passports
     putStrLn $ "Part1 Result: " ++ show (length validPassports)
+    let valid2 = filter isValid2 passports
+    putStrLn $ "Part2 Result: " ++ show (length valid2)
