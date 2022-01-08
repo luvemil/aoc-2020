d0bd7853352bb8a3d4351cb0c90ba42a5abebf1e
Marco Tarantino <taran.marco@gmail.com>
2022-01-08 19:06:55 +0100

    Update A21E17

--- src/AOC/A21/ES17.hs
+++ src/AOC/A21/ES17.hs
@@ -21,60 +21,11 @@ parseTarget = do
     y1 <- signedIntParser <* eof
     pure ((x0, x1), (y0, y1))
 
-computeSpeed :: Q -> Int -> [Q]
-computeSpeed (v0x, v0y) n =
-    let v0xSign = signum v0x
-        v0xs' = [v0x - (i * v0xSign) | i <- [0 .. min (abs v0x) n]]
-        v0xs'' = [last v0xs' | _ <- [min (abs v0x) n + 1 .. n]]
-        v0ys = [v0y - i | i <- [0 .. n]]
-     in zip (v0xs' ++ v0xs'') v0ys
-
-updatePos :: P -> Q -> P
-updatePos (x, y) (vx, vy) = (x + vx, y + vy)
-
-updatePosition :: P -> Q -> Int -> [P]
-updatePosition (x, y) q n =
-    let vs = computeSpeed q n
-     in F.foldl' (\acc cur -> acc ++ [last acc `updatePos` cur]) [(x, y)] vs
-
-fullUpdatePosition ::
-    -- | Initial position
-    P ->
-    -- | Initial velocity
-    Q ->
-    -- | Step to compute
-    Int ->
-    -- | (Position, Velocity)
-    S
-fullUpdatePosition p q 0 = (p, q)
-fullUpdatePosition p q n = last $ zip (updatePosition p q n) (computeSpeed q n)
-
--- let v0xSign = signum v0x
---     v0xs = [v0x - (i * v0xSign) | i <- [0 .. min (abs v0x) n]]
---     v0ys = [v0y - i | i <- [0 .. n]]
---  in (
---         ( x0 + sum v0xs
---         , y0 + sum v0ys
---         )
---     ,
---         ( last v0xs
---         , last v0ys
---         )
---     )
-
-computeFullPath :: P -> Q -> Target -> [S]
-computeFullPath p@(x, y) q t@((xs, xe), (ys, ye)) =
-    if x > max xs xe || y < min ys ye
-        then []
-        else
-            let (p', q') = fullUpdatePosition p q 1
-             in (p, q) : computeFullPath p' q' t
-
 isInTarget :: P -> Target -> Bool
 isInTarget (x, y) ((xs, xe), (ys, ye)) = x >= xs && x <= xe && y >= ys && y <= ye
 
-getAdmissiblePaths :: Target -> IO [[S]]
-getAdmissiblePaths t@((xs, xe), (ys, ye)) = do
+getAdmissiblePaths :: (P -> Q -> Target -> [S]) -> Target -> IO [[S]]
+getAdmissiblePaths cfp t@((xs, xe), (ys, ye)) = do
     let maxX = max (abs xs) (abs xe)
         maxY = max (abs ys) (abs ye)
         admissibleX = [x | x <- [- maxX .. maxX]]
@@ -83,14 +34,34 @@ getAdmissiblePaths t@((xs, xe), (ys, ye)) = do
     -- putStrLn $ "admissibleVelocities: " <> show admissibleVelocities
     let allPaths =
             admissibleVelocities
-                & map (\q -> computeFullPath (0, 0) q t)
+                & map (\q -> cfp (0, 0) q t)
                 & filter (not . null)
     -- putStrLn $ "allPaths: " <> show allPaths
     pure $ filter (\ps -> fst (last ps) `isInTarget` t) allPaths
 
+nextPoint :: P -> Q -> S
+nextPoint (px, py) (qx, qy) =
+    ( (px + qx, py + qy)
+    , (f qx, qy - 1)
+    )
+  where
+    f x = signum x * (abs x - 1)
+
+computePath :: P -> Q -> [S]
+computePath p q = (p, q) : computePath p' q'
+  where
+    (p', q') = nextPoint p q
+
+-- TODO: fix in case (xs, xe) is negative, or (ys, ye) is positive
+computePathUpTo :: P -> Q -> Target -> [S]
+computePathUpTo p q ((xs, xe), (ys, ye)) =
+    let maxX = max xs xe
+        minY = min ys ye
+     in takeWhile (\((x, y), _) -> x <= maxX && y >= minY) $ computePath p q
+
 part1 :: Target -> IO Int
 part1 t = do
-    admissiblePaths <- getAdmissiblePaths t
+    admissiblePaths <- getAdmissiblePaths computePathUpTo t
     -- putStrLn $ "Admissible paths: " <> show admissiblePaths
     let maximumPath = F.maximumBy (comparing (maximumOf (traversed . _1 . _2))) admissiblePaths
     putStrLn $ "Maximum path: " <> show maximumPath
@@ -99,7 +70,7 @@ part1 t = do
 
 part2 :: Target -> IO Int
 part2 t = do
-    admissiblePaths <- getAdmissiblePaths t
+    admissiblePaths <- getAdmissiblePaths computePathUpTo t
     pure $ length admissiblePaths
 
 main :: FilePath -> IO ()
