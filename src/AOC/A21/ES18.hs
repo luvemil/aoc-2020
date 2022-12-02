9c05d5eb3412b1035346ef1bade81f01b4e5f3a6
Marco Tarantino <taran.marco@protonmail.com>
2022-12-02 17:41:04 +0000

    A21E18

--- src/AOC/A21/ES18.hs
+++ src/AOC/A21/ES18.hs
@@ -1,26 +1,61 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module AOC.A21.ES18 where
+
+import AOC.Utils (Parser, embedMaybe, parseList)
+import Control.Applicative ((<|>))
 import Control.Lens
+import Control.Monad (forM_)
+import Data.Maybe (catMaybes)
+import Text.Megaparsec (MonadParsec (try), many, parseMaybe)
+import Text.Megaparsec.Char (char, digitChar)
+import Text.Read (readMaybe)
+import Data.Foldable (foldlM)
+
+-- Type definitions and accessors
 
 data SNum a = SRegular !a | SPair !(SNum a) !(SNum a)
   deriving (Show, Eq)
+
+data SBaseNum a = SBaseReg !a | SBasePair !a !a
+  deriving (Show, Eq)
+
+printSNum :: Show a => SNum a -> String
+printSNum (SRegular x) = show x
+printSNum (SPair x y) = "["<>printSNum x<> ","<>printSNum y<>"]"
+
+makePrisms ''SBaseNum
+
+liftSNum :: SBaseNum a -> SNum a
+liftSNum (SBaseReg x) = SRegular x
+liftSNum (SBasePair x y) = SPair (SRegular x) (SRegular y)
 
 tSVal :: Traversal (SNum a) (SNum b) a b -- Applicative f => (a -> f b) -> (SNum a) -> f (SNum b)
 tSVal f (SRegular x) = SRegular <$> f x
 tSVal f (SPair x y) = SPair <$> tSVal f x <*> tSVal f y
 
-data SPos = L | R
-  deriving (Show, Eq)
+tSPairVal :: Traversal (SNum a) (SNum b) (SBaseNum a) (SBaseNum b)
+tSPairVal f (SRegular x) = liftSNum <$> f (SBaseReg x)
+tSPairVal f (SPair (SRegular x) (SRegular y)) = liftSNum <$> f (SBasePair x y)
+tSPairVal f (SPair x y) =
+  let xV = tSPairVal f x
+      yV = tSPairVal f y
+   in SPair <$> xV <*> yV
 
-tSValIx :: IndexedTraversal [SPos] (SNum a) (SNum b) a b
-tSValIx p (SRegular x) = SRegular <$> indexed p [] x
-tSValIx p (SPair x y) = _g
+withDepth :: SNum a -> [(Int, SBaseNum a)]
+withDepth (SRegular x) = [(0, SBaseReg x)]
+withDepth (SPair (SRegular x) (SRegular y)) = [(0, SBasePair x y)]
+withDepth (SPair x y) = (withDepth x <> withDepth y) & traversed . _1 %~ \x -> x + 1
 
-sAdd :: SNum a -> SNum a -> SNum a
-sAdd = SPair
+itSPairVal :: IndexedFold Int (SNum a) (SBaseNum a)
+itSPairVal = ifolding withDepth
 
--- sReduce :: Num a => SNum a -> SNum a
--- sReduce (SRegular x) = SRegular x
--- sReduce _ = _f
+-- | Traverse an SNum in terms of regular values
+tSRegVal :: Traversal (SNum a) (SNum b) (SBaseNum a) (SBaseNum b)
+tSRegVal f (SRegular x) = liftSNum <$> f (SBaseReg x)
+tSRegVal f (SPair x y) = SPair <$> tSRegVal f x <*> tSRegVal f y
+
+-- Operations
 
 half :: Integral a => Bool -> a -> a
 half u x =
@@ -34,7 +69,133 @@ sSplit (SRegular x) =
     else SPair (SRegular (half False x)) (SRegular (half True x))
 sSplit y = y
 
+sBaseSplit :: Integral a => SBaseNum a -> SBaseNum a
+sBaseSplit (SBaseReg x) =
+  if x < 10
+    then SBaseReg x
+    else SBasePair (half False x) (half True x)
+sBaseSplit y = y
+
+data ReduceOp = Explode !Int | Split !Int | NoOp
+  deriving (Show, Eq)
+
+getExplodeOp :: SNum Int -> Maybe ReduceOp
+getExplodeOp sn = do
+  let xs = sn ^@.. itSPairVal
+  i <- findIndexOf (itraversed <. filteredBy (_2 . _SBasePair) . _1) (>= 4) xs
+  pure $ Explode i
+
+getSplitOp :: SNum Int -> Maybe ReduceOp
+getSplitOp sn = do
+  let xs = sn ^.. tSRegVal
+  i <- findIndexOf (itraversed <. _SBaseReg) (> 9) xs
+  pure $ Split i
+
+nextOp :: SNum Int -> ReduceOp
+nextOp sn =
+  head $
+    catMaybes
+      [ getExplodeOp sn,
+        getSplitOp sn,
+        Just NoOp
+      ]
+
+sNumSplitAt :: Int -> SNum Int -> SNum Int
+sNumSplitAt i sn =
+  let vals = sn ^.. tSRegVal
+      vals' = vals & itraversed %@~ \j x -> if j == i then sBaseSplit x else x
+   in sn & partsOf tSRegVal .~ vals'
+
+addRight :: SBaseNum Int -> Int -> SBaseNum Int
+addRight (SBaseReg x) t = SBaseReg $ x + t
+addRight (SBasePair x y) t = SBasePair x (y + t)
+
+addLeft :: SBaseNum Int -> Int -> SBaseNum Int
+addLeft (SBaseReg x) t = SBaseReg $ x + t
+addLeft (SBasePair x y) t = SBasePair (x + t) y
+
+sNumExplodeAt :: Int -> SNum Int -> SNum Int
+sNumExplodeAt i sn =
+  let vals = sn ^.. tSPairVal
+      -- v = preview (traversed . _SBasePair) $ take 1 . drop i $ vals
+      v = vals ^? taking 1 (dropping i traversed) . _SBasePair
+   in case v of
+        Nothing -> sn
+        Just (x, y) ->
+          let vals' = vals & itraversed %@~ \j theVal ->
+                if j == i -1 then addRight theVal x
+                else if j == i + 1 then addLeft theVal y
+                else if j == i then SBaseReg 0
+                else theVal
+           in sn & partsOf tSPairVal .~ vals'
+
+execOp :: SNum Int -> ReduceOp -> SNum Int
+execOp x NoOp = x
+execOp x (Explode i) = sNumExplodeAt i x
+execOp x (Split i) = sNumSplitAt i x
+
+reduceSNum :: SNum Int -> IO (SNum Int)
+reduceSNum x = case nextOp x of
+  NoOp -> do
+    -- putStrLn $ "[DEBUG] NOOP"
+    pure x
+  nOp -> do
+    -- putStrLn $ "[DEBUG] nextOp: " <> show nOp
+    let computed = execOp x nOp
+    -- putStrLn $ "[DEBUG] computed: " <> printSNum computed
+    if computed == x
+      then do
+        -- putStrLn $ "[DEBUG] Not updated"
+        fail "Not update"
+      else reduceSNum computed
+
+sAdd :: SNum Int -> SNum Int -> IO (SNum Int)
+sAdd x y = do
+  -- putStrLn $ "[DEBUG] sAdd: " <> printSNum x <> " + " <> printSNum y
+  let preReduced = SPair x y
+  -- putStrLn $ "[DEBUG] to reduce: " <> printSNum preReduced
+  reduceSNum $ preReduced
+
+magnitude :: SNum Int -> Int
+magnitude (SRegular x) = x
+magnitude (SPair x y) = 3 * magnitude x + 2 * magnitude y
+
+-- Parsers
+sRegularParser :: Parser (SNum Int)
+sRegularParser = do
+  l <- many digitChar
+  r :: Int <- embedMaybe . readMaybe $ l
+  pure $ SRegular r
+
+sNumParser :: Parser (SNum Int)
+sNumParser = do
+  _ <- char '['
+  l <- try sRegularParser <|> sNumParser
+  _ <- char ','
+  r <- try sRegularParser <|> sNumParser
+  _ <- char ']'
+  pure (SPair l r)
+
+-- Core
+
+part1 :: [SNum Int] -> IO Int
+part1 snums = do
+  sumNum <- foldlM sAdd (head (take 1 snums)) (drop 1 snums)
+  pure $ magnitude sumNum
+
+part2 :: [SNum Int] -> IO Int
+part2 snums = do
+  let pairs = concat [[(x, y), (y, x)] | x <- snums, y <- snums, y /= x ]
+  ms <- mapM (\(x, y) -> magnitude <$> sAdd x y) pairs
+  pure $ maximum ms
+
 main :: FilePath -> IO ()
 main fp = do
-  input <- readFile fp
-  putStrLn input
+  inputLines <- lines <$> readFile fp
+  snums <- parseList (embedMaybe . parseMaybe sNumParser) inputLines
+  -- forM_ snums print
+  res1 <- part1 snums
+  putStrLn $ "A21E18 - part1: " <> show res1
+
+  res2 <- part2 snums
+  putStrLn $ "A21E18 - part2: " <> show res2
--- test/AOC/A21/ES18Spec.hs
+++ test/AOC/A21/ES18Spec.hs
@@ -1,24 +1,143 @@
 module AOC.A21.ES18Spec where
 
 import AOC.A21.ES18
-import Test.Hspec
 import Control.Lens
+import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck
+import Text.Megaparsec (parseMaybe)
+import Data.Maybe (mapMaybe, isJust)
+import Control.Monad ((<=<), forM_)
+import Data.Foldable (Foldable(foldl'), foldlM)
+import AOC.Utils (embedMaybe)
 
 main :: IO ()
 main = hspec spec
 
+sRefEl :: String
+sRefEl = "[[4,5],[6,7]]"
+
 refEl :: SNum Int
 refEl = SPair (SPair (SRegular 4) (SRegular 5)) (SPair (SRegular 6) (SRegular 7))
+
+sRefEl2 :: String
+sRefEl2 = "[[[[4,5],6],[7,8]],9]"
+
+refEl2 :: SNum Int
+refEl2 =
+  SPair
+    ( SPair
+        ( SPair
+            (SPair (SRegular 4) (SRegular 5))
+            (SRegular 6)
+        )
+        ( SPair
+            (SRegular 7)
+            (SRegular 8)
+        )
+    )
+    (SRegular 9)
+
+refEl2Base :: [SBaseNum Int]
+refEl2Base = [SBasePair 4 5, SBaseReg 6, SBasePair 7 8, SBaseReg 9]
+
+refEl2BaseIxed :: [(Int, SBaseNum Int)]
+refEl2BaseIxed = [(3, SBasePair 4 5), (3, SBaseReg 6), (2, SBasePair 7 8), (1, SBaseReg 9)]
+
+sRegNum :: Gen (SNum Int)
+sRegNum = SRegular <$> chooseInt (0, 9)
+
+sNumDepth :: Int -> Gen (SNum Int)
+sNumDepth x
+  | x < 0 =
+    sNumDepth 0
+  | x > 4 = sNumDepth 4
+  | x == 0 = sRegNum
+  | otherwise = do
+    l <- sNumDepth (x - 1)
+    r <- sNumDepth (x - 1)
+    pure $ SPair l r
+
+instance Arbitrary (SNum Int) where
+  arbitrary = sized sNumDepth
+
+prop_indexedSameAsTrav :: SNum Int -> Bool
+prop_indexedSameAsTrav snum =
+  let iVal = snum ^.. itSPairVal
+      tVal = snum ^.. tSPairVal
+   in iVal == tVal
+
+prop_regSameAsNaive :: SNum Int -> Bool
+prop_regSameAsNaive snum =
+  let naive = snum ^.. tSVal . to SBaseReg
+      reg = snum ^.. tSRegVal
+    in naive == reg
+
+opsVals :: [(String, ReduceOp)]
+opsVals = [
+    ("[[[[[9,8],1],2],3],4]", Explode 0),
+    ("[7,[6,[5,[4,[3,2]]]]]", Explode 4),
+    ("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", Explode 6),
+    ("[[3,[2,[8,0]]],[10,[5,[7,0]]]]", Split 4)
+  ]
+
+fromString :: MonadFail m => String -> m (SNum Int)
+fromString = embedMaybe . parseMaybe sNumParser
+
+sumsVals :: [([String], String)]
+sumsVals =
+  [ (["[1,1]","[2,2]","[3,3]","[4,4]"], "[[[[1,1],[2,2]],[3,3]],[4,4]]")
+  , (["[1,1]","[2,2]","[3,3]","[4,4]","[5,5]"], "[[[[3,0],[5,3]],[4,4]],[5,5]]")
+  , (["[1,1]","[2,2]","[3,3]","[4,4]","[5,5]","[6,6]"], "[[[[5,0],[7,4]],[5,5]],[6,6]]")
+  ]
 
 spec :: Spec
 {-# NOINLINE spec #-}
 spec =
   describe "operations" $ do
-    it "split" $
+    it "sSplit splits correctly" $
       map sSplit [SRegular 10, SRegular 11, SRegular 12]
         `shouldBe` [ SPair (SRegular 5) (SRegular 5),
                      SPair (SRegular 5) (SRegular 6),
                      SPair (SRegular 6) (SRegular 6)
                    ]
-    it "gets all values" $ do
-        (refEl ^.. tSVal) `shouldBe` [4, 5, 6, 7]
+    it "sBaseSplit splits correctly" $
+      map sBaseSplit [SBaseReg 10, SBaseReg 11, SBaseReg 12]
+        `shouldBe` [SBasePair 5 5, SBasePair 5 6, SBasePair 6 6]
+    it "tSVal gets all values" $ do
+      (refEl ^.. tSVal) `shouldBe` [4, 5, 6, 7]
+    it "tSPairVal gets all values" $ do
+      (refEl2 ^.. tSPairVal) `shouldBe` refEl2Base
+    it "itSPairVal gets values with depth" $ do
+      (refEl2 ^@.. itSPairVal) `shouldBe` refEl2BaseIxed
+    it "parses el1" $ do
+      parseMaybe sNumParser sRefEl `shouldBe` Just refEl
+    it "parses el2" $ do
+      parseMaybe sNumParser sRefEl2 `shouldBe` Just refEl2
+    prop "tSPairVal is the same as itSPairVal" $ do
+      prop_indexedSameAsTrav
+    prop "tSRegVal is the same as (tSVal . to SBaseReg)" $ do
+      prop_regSameAsNaive
+    it "explodeAt worked" $ do
+      el <- fromString "[[[[0,[3,2]],[3,3]],[4,4]],[5,5]]"
+      res <- fromString "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+      pos1Res <- fromString "[3,2]"
+      let vals = el ^.. tSPairVal
+      vals `shouldBe` [SBaseReg 0, SBasePair 3 2, SBasePair 3 3, SBasePair 4 4, SBasePair 5 5]
+      let pos1 = vals ^? taking 1 (dropping 1 traversed)
+      liftSNum <$> pos1 `shouldBe` Just pos1Res
+      sNumExplodeAt 1 el `shouldBe` res
+    it "nextOp works" $ do
+      mapMaybe (Just . nextOp <=< parseMaybe sNumParser . fst) opsVals `shouldBe` map snd opsVals
+    it "sAdd trivial works" $ do
+      sn1 <- embedMaybe $ parseMaybe sNumParser "[1,1]"
+      sn2 <- embedMaybe $ parseMaybe sNumParser "[2,2]"
+      res <- embedMaybe $ parseMaybe sNumParser "[[1,1],[2,2]]"
+      sAddRes <- sAdd sn1 sn2
+      sAddRes `shouldBe` res
+    forM_ sumsVals $ \(sNumsStr, sResStr) ->
+      it ("sAdd: " <> show sNumsStr <> " -> " <> sResStr) $ do
+        let sNums = mapMaybe (parseMaybe sNumParser) sNumsStr
+        sRes <- embedMaybe $ parseMaybe sNumParser sResStr
+        sSum <- foldlM sAdd (head (take 1 sNums)) (drop 1 sNums)
+        sSum `shouldBe` sRes
