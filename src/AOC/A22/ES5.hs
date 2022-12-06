d56c96d78a73df54fc94940a085b63d81d76d9cc
Marco Tarantino <taran.marco@protonmail.com>
2022-12-06 22:13:01 +0000

    A22E5

--- src/AOC/A22/ES5.hs
+++ src/AOC/A22/ES5.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 module AOC.A22.ES5 where
 
 import AOC.Utils
@@ -5,19 +6,30 @@ import Control.Lens
 import Control.Monad (forM, forM_, replicateM_)
 import qualified Data.List as L
 import qualified Data.Map.Strict as M
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, fromJust)
 import Text.Megaparsec
 import Text.Megaparsec.Char (alphaNumChar, char, digitChar, string)
 import Text.Read (readMaybe)
+import Data.Foldable (foldl')
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 newtype Stack a = Stack {unStack :: [a]}
   deriving (Eq)
   deriving newtype (Show)
 
+makePrisms ''Stack
+
 buildStacks :: [(Char, [Maybe Char])] -> M.Map Char (Stack Char)
 buildStacks listed =
   let p = listed & traversed . _2 %~ Stack . catMaybes
    in M.fromList p
+
+popStack :: Int -> Stack Char -> (Stack Char, [Char])
+popStack n (Stack cs) = (Stack (drop n cs), take n cs)
+
+pushStack :: [Char] -> Stack Char -> Stack Char
+pushStack cs (Stack cs') = Stack (cs <> cs')
 
 -- Parsers
 
@@ -53,9 +65,38 @@ parseAction = do
   endLabel :: Char <- alphaNumChar
   pure (qty, startLabel, endLabel)
 
-part1 :: M.Map Char (Stack Char) -> (Int, Char, Char) -> IO Int
+-- Actions
+
+evalAction :: M.Map Char (Stack Char) -> (Int, Char, Char) -> M.Map Char (Stack Char)
+evalAction s (qty, startLabel, endLabel) =
+    -- s & itraversed  %@~ \i x -> x
+    let (startStack, vals) = fromJust $ popStack qty <$> s ^? ix startLabel
+     in s & ix startLabel .~ startStack
+          & ix endLabel %~ pushStack (reverse vals)
+
+evalAction2 :: M.Map Char (Stack Char) -> (Int, Char, Char) -> M.Map Char (Stack Char)
+evalAction2 s (qty, startLabel, endLabel) =
+    -- s & itraversed  %@~ \i x -> x
+    let (startStack, vals) = fromJust $ popStack qty <$> s ^? ix startLabel
+     in s & ix startLabel .~ startStack
+          & ix endLabel %~ pushStack vals
+
+-- Parts
+
+part1 :: M.Map Char (Stack Char) -> [(Int, Char, Char)] -> IO String
 part1 stacks actions = do
-    pure undefined
+    let finalStacks = foldl' evalAction stacks actions
+        res' = sortBy (comparing fst) $ finalStacks ^@.. itraversed
+        res = res' ^.. traversed . _2 . _Stack . to head
+    pure res
+
+
+part2 :: M.Map Char (Stack Char) -> [(Int, Char, Char)] -> IO String
+part2 stacks actions = do
+    let finalStacks = foldl' evalAction2 stacks actions
+        res' = sortBy (comparing fst) $ finalStacks ^@.. itraversed
+        res = res' ^.. traversed . _2 . _Stack . to head
+    pure res
 
 main :: FilePath -> IO ()
 main fp = do
@@ -79,4 +120,8 @@ main fp = do
 
   let stacks = buildStacks $ zip labels (L.transpose stackItems)
 
-  putStrLn $ "AOC - part1: "
+  res1 <- part1 stacks actions
+  putStrLn $ "AOC - part1: " <> res1
+
+  res2 <- part2 stacks actions
+  putStrLn $ "AOC - part2: " <> res2
