22b0c81c4ff2c9fe7ee3793a78ec57be683e5b40
Marco Tarantino <taran.marco@protonmail.com>
2022-12-09 22:13:33 +0000

    A22E08 slow implementation

--- src/AOC/A22/ES8.hs
+++ src/AOC/A22/ES8.hs
@@ -8,8 +8,10 @@ import Control.Lens
 import Control.Monad.Reader
 import Control.Monad.State
 import Data.Foldable (concatMap, foldl')
+import qualified Data.Foldable as L
 import Data.List (sortBy)
-import Data.Maybe (fromJust)
+import qualified Data.Map.Strict as M
+import Data.Maybe (fromJust, fromMaybe)
 import Data.Ord (comparing)
 
 type Comp a =
@@ -35,11 +37,11 @@ step cur = do
       if cur > prev
         then do
           modify' $ \(_, _, lVis', _) -> (pos + 1, Just cur, lVis' <> [pos], [(pos, cur)])
-        else
-          modify' $ \(_, p, lVis', rVis') -> (pos + 1, p, lVis', filter (\(_, a) -> a > cur) rVis' <> [(pos, cur)])
-          -- if cur < prev
-          --   then modify' $ \(_, prevM, lVis', rVis') -> (pos + 1, prevM, lVis', cur : rVis')
-          --   else modify' $ \(_, prevM, lVis', _) -> (pos + 1, prevM, lVis', [cur])
+        else modify' $ \(_, p, lVis', rVis') -> (pos + 1, p, lVis', filter (\(_, a) -> a > cur) rVis' <> [(pos, cur)])
+
+-- if cur < prev
+--   then modify' $ \(_, prevM, lVis', rVis') -> (pos + 1, prevM, lVis', cur : rVis')
+--   else modify' $ \(_, prevM, lVis', _) -> (pos + 1, prevM, lVis', [cur])
 
 visiblePos :: Ord a => [a] -> [Int]
 visiblePos [] = []
@@ -48,7 +50,36 @@ visiblePos xs =
       rVis = map fst rVisItems
    in uniq $ lVis <> rVis
 
+-- Stupid implementation: given an index compute its visibility
+rowVisibility :: Ord a => Int -> [a] -> (Int, Int)
+rowVisibility pos xs =
+  let val = fromJust $ xs !? pos
+      xs' =
+        xs
+          & (itraversed %@~ \i x -> (i, x >= val))
+          & filter snd
+          & map fst
+      lowIx = maximum $ filter (< pos) xs' <> [0]
+      highIx = minimum $ filter (> pos) xs' <> [length xs - 1]
+   in (pos - lowIx, highIx - pos)
+
+rowVisibilities :: Ord a => [a] -> [(Int, Int)]
+rowVisibilities xs = xs & itraversed %@~ \i _ -> rowVisibility i xs
+
+allRowVisiblities :: Ord a => Grid a -> Grid Int
+allRowVisiblities grid =
+  let byRow = map rowVisibilities $ grid ^.. _rows
+      byCol = map rowVisibilities $ grid ^.. _cols
+   in grid & itraversed %@~ \(x, y) _ -> 
+        let br = byRow & preview (ix y) & preview (_Just . ix x) & fromJust & uncurry (*)
+            bc = byCol & preview (ix x) & preview (_Just . ix y) & fromJust & uncurry (*)
+         in br * bc
+
 -- Parts
+part2 :: Grid Int -> IO Int
+part2 g = do
+  let arv = allRowVisiblities g
+  pure . fromJust $ maximumOf traversed arv
 
 part1 :: Grid Int -> IO Int
 part1 g@(Grid w h _) = do
@@ -64,12 +95,53 @@ part1 g@(Grid w h _) = do
       visByCol' = g ^.. _cols . to visiblePos
       visByRow' = g ^.. _rows . to visiblePos
   -- putStrLn "visible positions by column"
-  -- forM_ visByCol' print  
+  -- forM_ visByCol' print
   -- putStrLn "----"
   -- putStrLn "visible positions by row"
-  -- forM_ visByRow' print  
+  -- forM_ visByRow' print
   -- putStrLn "----"
   pure $ length allVisible
+
+printGridEdge :: Grid a -> (Int, Int) -> Char
+printGridEdge _ (0, 0) = '+'
+printGridEdge (Grid w _ _) (x, 0) = if x == w - 1 then '+' else '-'
+printGridEdge (Grid _ h _) (0, y) = if y == h - 1 then '+' else '|'
+printGridEdge (Grid w h _) (x, y)
+  | x == w - 1 && y == h - 1 = '+'
+  | x == w - 1 = '|'
+  | y == h - 1 = '-'
+  | otherwise = ' '
+
+printLevels :: Grid Int -> IO ()
+printLevels grid = do
+  forM_ (reverse [0 .. 9]) $ \size -> do
+    let g9 =
+          grid & itraversed %@~ \i x ->
+            if x >= size
+              then 'X'
+              else printGridEdge grid i
+    putStrLn $ "=== Size " <> show size <> " ==="
+    putStrLn $ prettyGrid g9
+
+printProfile :: Int -> [Int] -> IO ()
+printProfile l xs = do
+  let initialMap :: M.Map Int [Int]
+      initialMap = M.fromList $ map (,[]) [0 .. 9]
+      initialString = [' ' | _ <- [0 .. l]]
+      heights =
+        xs
+          & itraversed %@~ (\i x -> (x, i))
+          & L.foldl'
+            ( \acc (i, x) ->
+                let base = acc & M.insertWith (\nv ov -> ov <> nv) i [x]
+                 in L.foldl' (\acc' j -> M.insertWith (\nv ov -> ov <> nv) j [x] acc') base [0 .. i]
+            )
+            initialMap
+
+  forM_ (reverse [0 .. 9]) $ \size -> do
+    let positions = fromMaybe [] $ M.lookup size heights
+        finalS = L.foldl' (\acc cur -> acc & ix cur .~ 'x') initialString positions
+    putStrLn finalS
 
 main :: FilePath -> IO ()
 main fp = do
@@ -78,5 +150,13 @@ main fp = do
   grid <- createGrid parsed
 
   res1 <- part1 grid
-
   putStrLn $ "A22E8 - part1: " <> show res1
+
+  -- Print the levels on x-y
+  -- printLevels grid
+  -- let (Grid w _ _) = grid
+  --     items = grid ^.. _cols . ix 15
+  -- printProfile w items
+
+  res2 <- part2 grid
+  putStrLn $ "A22E8 - part2: " <> show res2
